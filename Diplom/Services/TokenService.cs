3450b14bde66d64d49b62559f27cceaf757c7aec
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-04-11 16:25:12 +0500

    implementation tokenservice

---
+++ Diplom/Services/TokenService.cs
@@ -0,0 +1,45 @@
+﻿using Diplom.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Diplom.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly  UserManager<AppUser> _userManager;
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration conf, UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(conf["TokenKey"]));
+        }
+        public async Task<string> CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
+            };
+            var roles = await _userManager.GetRolesAsync(user);
+
+            claims.AddRange(roles.Select(role=> new Claim(ClaimTypes.Role, role)));
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
