fd8e965ee37e200d7a86add28e0005ef60f05076
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-04-12 16:08:57 +0500

    implementation login

---
+++ Diplom/Services/AuthService.cs
@@ -0,0 +1,44 @@
+﻿using Diplom.Data;
+using Diplom.DTO.AuthDtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Diplom.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
+       
+        public AuthService(ITokenService tokenService, UserManager<AppUser> userManager)
+        {
+
+            _tokenService = tokenService;
+            _userManager = userManager;
+
+        }
+        public async Task<UserDto> Login(LoginDto loginRequest)
+        {
+            var user = await _userManager.Users.SingleOrDefaultAsync(x=>x.UserName == loginRequest.UserName);
+            bool checkPassword = await _userManager.CheckPasswordAsync(user, loginRequest.Password);
+            if (user == null||checkPassword==false)
+            {
+                return null;
+            }
+            var token = await _tokenService.CreateToken(user);
+
+            var userToReturn = new UserDto
+            {
+                UserName = loginRequest.UserName,
+                Token = token,
+            };
+
+            return userToReturn;
+        }
+
+        public Task<UserDto> Register(RegisterDto registerDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
