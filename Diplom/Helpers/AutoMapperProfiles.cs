6dd7e2db3f932f89a729836b02a6c4841bf34e92
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-04-15 17:37:28 +0500

    Comments for AutoMapperProfiles

--- Diplom/Helpers/AutoMapperProfiles.cs
+++ Diplom/Helpers/AutoMapperProfiles.cs
@@ -10,15 +10,22 @@ namespace Diplom.Helpers
     {
         public AutoMapperProfiles()
         {
+            // Маппинг из DistrictsEn в DistrictDto
             CreateMap<DistrictsEn, DistrictDto>();
+
+            // Маппинг из DistrictCreateUpdateDto в DistrictsEn
             CreateMap<DistrictCreateUpdateDto, DistrictsEn>();
 
-
+            // Маппинг из RouteEn в RouteEnDto
+            // Переопределение некоторых полей с помощью ForMember
             CreateMap<RouteEn, RouteEnDto>()
                 .ForMember(d => d.StarPoint, o => o.MapFrom(s => s.StartPoint.Title))
                 .ForMember(d => d.EndPoint, o => o.MapFrom(s => s.EndPoint.Title));
-
+            
+            // Маппинг из RouteCreateDto в RouteEn
             CreateMap<RouteCreateDto, RouteEn>();
+
+            // Маппинг из RouteUpdateDto в RouteEn
             CreateMap<RouteUpdateDto, RouteEn>();
         }
     }
