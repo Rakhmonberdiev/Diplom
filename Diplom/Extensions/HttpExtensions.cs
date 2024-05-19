a28c967b6623ed59e189872c25dda9d6da766646
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-05-19 12:50:05 +0500

    pagination params and httpextension

---
+++ Diplom/Extensions/HttpExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Diplom.Helpers;
+using System.Text.Json;
+
+namespace Diplom.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response, PaginationHeader header)
+        {
+            var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(header, jsonOptions));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
